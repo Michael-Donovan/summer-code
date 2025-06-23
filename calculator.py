641c1b2ef5d43497bffed4512aeb30b06c65b508
Michael-Donovan <mikevdonovan7@gmail.com>
2025-06-23 14:43:39 +0000

    ko

---
+++ calculator.py
@@ -0,0 +1,16 @@
+while True:
+    operation=input("what operation fo you want")
+    num1=int(input("what number do you want to use"))
+    num2=int(input("what number do you want to use"))
+    if operation=="+":
+        print(num1+num2)
+    if operation=="-":
+        print(num1-num2)
+    if operation=="*":
+        print(num1*num2)
+    if operation=="/":
+    print(num1/num2)
+    Continue=input("do you want to stop the  program (yes or no)")
+    if Continue=="yes":
+        break
+        
