aa564b6128ee92ecf69b49fba9ae2b4ff4f8a946
Michael-Donovan <mikevdonovan7@gmail.com>
2025-06-23 17:07:13 +0000

    finished guessing game

--- guessing game algorithem.py
+++ guessing game algorithem.py
@@ -2,15 +2,33 @@ import random
 num=random.randint(1,100)
 start=0
 end=100
+counter=0
 while True:
-    guess=int(end/2+start)
-    print(guess)
+    counter+=1
+    guess=int((end-start)/2)+start
     if guess>num:
-        print("the number is lower")
         end=guess-1
     elif guess<num:
-        print("the number is higher")
         start=guess+1
     else: 
-        print(" you win")
         break
+counter2=0
+while True:
+    counter2+=1
+    guess=int(input("enter a number 1/100 "))
+    if guess>num:
+        print("the number is lower")
+    elif guess<num:
+        print("the number is higher")
+    else: 
+        break
+print(counter)
+print(counter2)
+if counter>counter2:
+    print("player 1 has won the game")
+
+elif counter2>counter: 
+    print("player two has won the game")
+
+else: 
+    print("there has been a tie")
