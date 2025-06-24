560e731efa1103822ccd48ab1902f1e2e86d81b4
Michael-Donovan <mikevdonovan7@gmail.com>
2025-06-24 13:45:44 +0000

    square

--- counting.py
+++ counting.py
@@ -1,7 +1,2 @@
-i=0
-while i<101:
-    if i%6==0:
-        i+=1
-        continue
+for i in range (1,103,3):
     print(i)
-    i=1+i
---
+++ shapes.py
@@ -0,0 +1,6 @@
+size=int(input("how many lines would you like the square to be?"))
+for i in range(size):
+    for x in range(size):
+        print("*  ", end="")
+    print()
+
